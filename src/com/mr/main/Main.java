5ff3bd23e643c5ac77efd9efa8ed459aea746581
doomer0923 <59193921+doomer0923@users.noreply.github.com>
2020-06-03 22:57:19 +0800

    e

---
+++ src/com/mr/main/Main.java
@@ -0,0 +1,23 @@
+package com.mr.main;
+
+import com.mr.draw.DrawPictureFrame;
+
+/**
+ * 主类
+ * 
+ * @开发人 申小琦
+ * @更新时间 2016年08月01日
+ * @开发单位 吉林省明日科技有限公司
+ * @公司网址 www.mingribook.com
+ */
+public class Main {
+	/**
+	 * 主方法
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DrawPictureFrame frame = new DrawPictureFrame();
+		frame.setVisible(true);
+	}
+}
